52c4ffc84fefe1e8e9824b65494b289c59596033
ahmtydn <ahmttyydn@gmail.com>
2025-10-06 21:20:38 +0300

    fix: update include statements for isar_flutter_libs_plugin in generated_plugin_registrant files

--- packages/isar_test/windows/flutter/generated_plugin_registrant.cc
+++ packages/isar_test/windows/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,7 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <isar_plus_flutter_libs/isar_plus_flutter_libs_plugin.h>
+#include <isar_plus_flutter_libs/isar_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   IsarFlutterLibsPluginRegisterWithRegistrar(
