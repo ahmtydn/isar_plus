ea3025cc68bd0704f7ef0cd17955808c8e81a79e
ahmtydn <ahmttyydn@gmail.com>
2025-10-06 21:29:01 +0300

    fix: remove unused media import from tweet.dart

--- packages/isar_plus_test/lib/src/twitter/tweet.dart
+++ packages/isar_plus_test/lib/src/twitter/tweet.dart
@@ -1,7 +1,6 @@
 import 'package:isar_plus/isar_plus.dart';
 import 'package:isar_plus_test/src/twitter/entities.dart';
 import 'package:isar_plus_test/src/twitter/geo.dart';
-import 'package:isar_plus_test/src/twitter/media.dart';
 import 'package:isar_plus_test/src/twitter/user.dart';
 import 'package:isar_plus_test/src/twitter/util.dart';
 import 'package:json_annotation/json_annotation.dart';
