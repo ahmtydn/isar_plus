00f9737271c074a18e983189eec46c234e149665
Ahmet Aydın <84200491+ahmtydn@users.noreply.github.com>
2026-02-15 01:58:36 +0300

    refactor: hide kIsWeb from flutter/foundation.dart import to resolve potential conflicts.

--- packages/isar_plus_test/integration_test/isar_test.dart
+++ packages/isar_plus_test/integration_test/isar_test.dart
@@ -2,7 +2,7 @@
 
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' hide kIsWeb;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:isar_plus_test/isar_plus_test.dart';
